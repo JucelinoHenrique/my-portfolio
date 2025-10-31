4edba74420a491329a9bc51a131d7e17c8d1b89a
JucelinoHenrique <102430350+JucelinoHenrique@users.noreply.github.com>
2025-10-31 11:53:10 -0300

    style(contact): implemented contact button and resume

--- my-portfolio/src/pages/components/Contact.tsx
+++ my-portfolio/src/pages/components/Contact.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import Link from 'next/link';
+import { useState } from 'react';
 import { IconType } from 'react-icons';
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import ContactModal from './ProjectButton/ContactModal';
 
 type Social = {
   label: string;
@@ -11,28 +13,30 @@ type Social = {
 };
 
 const socials: Social[] = [
-  { label: 'Instagram',   href: 'https://www.instagram.com/juce.17', Icon: FaInstagram },
-  { label: 'LinkedIn',    href: 'https://www.linkedin.com/in/jucelino-oliveira-4109a9229/', Icon: FaLinkedin },
-  { label: 'Github',    href: 'https://github.com/JucelinoHenrique', Icon: FaGithub }
+  { label: 'Instagram', href: 'https://www.instagram.com/juce.17', Icon: FaInstagram },
+  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/jucelino-oliveira-4109a9229/', Icon: FaLinkedin },
+  { label: 'Github', href: 'https://github.com/JucelinoHenrique', Icon: FaGithub }
 ];
 
 const nav = [
   { label: 'About Me', href: '#about' },
-  { label: 'Resume',   href: '/resume' },
+  { label: 'Resume', href: '/resume' },
   { label: 'Contacts', href: '#contact' },
 ];
 
 export default function Footer() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <footer className="w-full border-t border-white/5 bg-[#0f0f0f]">
       <div className="mx-auto max-w-6xl px-6 py-10 text-sm text-neutral-300">
         {/* Marca */}
         <div className="flex items-center justify-center gap-3">
-           <h1 className="text-xl font-bold tracking-tight text-white flex items-center">
-              <span className={'text-[#62040e] text-2xl'}>&lt;</span>
-              Jucelino<span className="text-white">.Dev</span>
-              <span className={'text-[#62040e] text-2xl'}>/&gt;</span>
-            </h1>
+          <h1 className="text-xl font-bold tracking-tight text-white flex items-center">
+            <span className={'text-[#62040e] text-2xl'}>&lt;</span>
+            Jucelino<span className="text-white">.Dev</span>
+            <span className={'text-[#62040e] text-2xl'}>/&gt;</span>
+          </h1>
         </div>
 
         {/* Socials (ícones 40x40, redondos e simétricos) */}
@@ -54,17 +58,30 @@ export default function Footer() {
 
         {/* Navegação */}
         <nav className="mt-6 flex flex-wrap items-center justify-center gap-x-8 gap-y-3">
-          {nav.map((item) => (
-            <Link
-              key={item.label}
-              href={item.href}
-              className="text-neutral-300 transition hover:text-brandGreen focus:outline-none focus:ring-2 focus:ring-brandGreen/70 rounded"
-            >
-              {item.label}
-            </Link>
-          ))}
+          <button
+            type="button"
+
+            className="inline-flex items-center justify-center rounded-lg bg-[#6b0713] px-5 py-2.5 text-white text-sm font-medium shadow-sm
+         hover:bg-[#6b0713]/90 hover:shadow-md
+         focus:outline-none focus:ring-4 focus:ring-[#6b0713]/30
+         active:translate-y-px transition"
+          >
+            <a href="/Currículo_Jucelino_Henrique.pdf" target="_blank" rel="noopener noreferrer" >Baixar Currículo</a>      
+          </button>
+
+          <button
+             onClick={() => setIsOpen(true) }
+            type="button"
+            className="inline-flex items-center justify-center rounded-lg border border-[#6b0713] px-5 py-2.5 text-white text-sm font-medium
+         hover:bg-[#6b0713]/10
+         focus:outline-none focus:ring-4 focus:ring-[#6b0713]/20
+         active:translate-y-px transition"
+          >
+            Entrar em contato
+          </button>
         </nav>
       </div>
+      <ContactModal isOpen={isOpen} onClose={()=> setIsOpen(false)}/>
     </footer>
   );
 }
